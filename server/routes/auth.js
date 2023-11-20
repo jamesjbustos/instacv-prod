b2211229c883f6131a8f6366f59f1cd37650dce1
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-11-19 23:56:32 -0500

    test original url

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const CLIENT_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://instacv.jamesjbustos.com'
+    ? 'https://instacv-prod-client.up.railway.app'
     : 'http://localhost:5173';
 
 router.get('/login/success', (req, res) => {
